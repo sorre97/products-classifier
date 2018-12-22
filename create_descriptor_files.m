4dd6ab1fc9d9076bfb53a3f09babc3938fce6ade
sorre97 <sorre97@hotmail.it>
2018-12-22 01:20:08 +0100

    dataset for coca-cola, read list and label list for coca-cola

--- create_descriptor_files.m
+++ create_descriptor_files.m
@@ -6,15 +6,17 @@ function create_descriptor_files()
   nimages = numel(images);
   
   %%descriptors array
+  m = [];
 
   for n = 1 : nimages
     
     im = imread(['dataset/' images{n}]);
     
     %%descriptors calculator
+    m = [m; mean(im(:))];
     
   end
      
-  save('descriptors.mat', 'images', 'labels', 'DESCRIPTORS');
+  save('descriptors.mat', 'images', 'labels', 'm');
 
 end
