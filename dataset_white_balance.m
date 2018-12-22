82f598ba560ce82324b3cfaec256d0bf35153ca4
sorre97 <sorre97@hotmail.it>
2018-12-22 03:37:26 +0100

    icecream and cocacola photos for dataset, script for white balacing dataset images and renaming

---
+++ dataset_white_balance.m
@@ -0,0 +1,24 @@
+% loads photos, white balances them and renames according to counter
+d = uigetdir(pwd, 'prova');
+files = dir(fullfile(d, '*.JPG'));
+
+COUNTER = 100;
+for i = 1 : numel(files)
+   image = files(i);
+   im = imread([image.folder '/' image.name]);
+   
+   imw = white_balance(im);
+   if(COUNTER < 10)
+       filename = ['000' num2str(COUNTER)];
+   elseif(COUNTER < 100)
+       filename = ['00' num2str(COUNTER)];
+   elseif(COUNTER < 1000)
+       filename = ['0' num2str(COUNTER)];
+   else
+       filename = num2str(COUNTER);
+   end
+   
+   COUNTER = COUNTER + 1;
+   
+   imwrite(imw, [image.folder '/' filename '.JPG']);
+end
--- train_classifier.m
+++ train_classifier.m
@@ -1,14 +1,10 @@
-function train_classifier()
-
 load('descriptors.mat');
 
 % partitioning labels in 80% tranining and 20% test
-%cv = cvpartition(labels, 'Holdout', 0.2);
+cv = cvpartition(labels, 'Holdout', 0.2);
 
-%result = test_classifier(hu, labels, cv);
+result = test_classifier(hu, labels, cv);
 
-%perf = [result.accuracy];
+perf = [result.accuracy];
     
-%figure, bar(perf), xlabel("Number of try"), ylabel("Accuracy on test");
-
-end
+figure, bar(perf), xlabel("Number of try"), ylabel("Accuracy on test");
