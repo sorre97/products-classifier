f217b0cce697592997a55c25f612516b9cfbbda3
sorre97 <sorre97@hotmail.it>
2019-01-31 13:06:00 +0100

    Documentation

--- classificators/train_macro_classifier.m
+++ classificators/train_macro_classifier.m
@@ -1,7 +1,6 @@
 load('descriptors/descriptors.mat');
 
-%descriptor_vector = [hu compactness color CEDD];
-descriptor_vector = [hu CEDD];
+descriptor_vector = [hu compactness color CEDD];
 
 %% feature incorrelation
 Y=tsne(descriptor_vector);
